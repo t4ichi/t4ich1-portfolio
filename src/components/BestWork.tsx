c1189961f73ff37e59214736eab30fe71d30b5c4
t4ichi <taichinoheya020316@icloud.com>
2023-02-25 21:14:45 +0900

    BestWorkの遷移先のURLをGitHubに変更

--- src/components/BestWork.tsx
+++ src/components/BestWork.tsx
@@ -5,7 +5,7 @@ export const BestWorks = () => {
     return(
         <Container className='d-flex align-items-center justify-content-center'>
             <Card style={{ width: '40rem'}}>
-                <a href="https://itunes.apple.com/jp/app/id1609216148?mt=8"><Card.Img variant="top" src="/work/animal-search.png"/></a>
+                <a href="https://github.com/t4ichi/AnimalSearchOnline"><Card.Img variant="top" src="/work/animal-search.png"/></a>
                 <Card.Body>
                     <Card.Title className="d-flex justify-content-center fw-bold">どうぶつ推理-オンライン対戦ゲーム-</Card.Title>
                     <Card.Text className="d-flex justify-content-center">オンライン対戦ができるパズルゲーム</Card.Text>
